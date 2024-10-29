5653591deb2217c6ae90d7a21e9698c26ad7cb33
Sergey <mr.ultimateground@gmail.com>
2024-10-29 16:40:02 +0100

    is_default_constructor test added is_integral_constructor test added

--- main.cpp
+++ main.cpp
@@ -1,6 +1,23 @@
 #include<iostream>
+#include<limits.h>
 #include<catch.hpp>
 #include "UnsignedBigInteger.h"
+
+TEST_CASE("constructors") {
+	SECTION("is_default_constuctor") {
+		REQUIRE_NOTHROW(UnsignedBigInteger());
+	}
+	SECTION("is_integral_constructor") {
+		REQUIRE_NOTHROW(UnsignedBigInteger(1));
+		REQUIRE_THROWS(UnsignedBigInteger(-1));
+		constexpr int int_max = std::numeric_limits<int>::max();
+		REQUIRE_NOTHROW(UnsignedBigInteger(int_max));
+		constexpr long long_max = std::numeric_limits<long>::max();
+		REQUIRE_NOTHROW(UnsignedBigInteger(long_max));
+		constexpr long long long_long_max = std::numeric_limits<long>::max();
+		REQUIRE_NOTHROW(UnsignedBigInteger(long_long_max));
+	}
+}
 
 int main(int argc, char* argv[]) {
 	int result = Catch::Session().run(argc, argv);

988f717a639726d88ea1c42126265fa7e3a2af69
Sergey <mr.ultimateground@gmail.com>
2024-10-30 15:39:03 +0100

    is_eaual operator test added

--- main.cpp
+++ main.cpp
@@ -4,19 +4,35 @@
 #include "UnsignedBigInteger.h"
 
 TEST_CASE("constructors") {
-	SECTION("is_default_constuctor") {
+	SECTION("is default constuctor") {
 		REQUIRE_NOTHROW(UnsignedBigInteger());
 	}
-	SECTION("is_integral_constructor") {
+	SECTION("is integral constructor") {
 		REQUIRE_NOTHROW(UnsignedBigInteger(1));
 		REQUIRE_THROWS(UnsignedBigInteger(-1));
 		constexpr int int_max = std::numeric_limits<int>::max();
 		REQUIRE_NOTHROW(UnsignedBigInteger(int_max));
 		constexpr long long_max = std::numeric_limits<long>::max();
 		REQUIRE_NOTHROW(UnsignedBigInteger(long_max));
-		constexpr long long long_long_max = std::numeric_limits<long>::max();
+		constexpr long long long_long_max = std::numeric_limits<long long>::max();
 		REQUIRE_NOTHROW(UnsignedBigInteger(long_long_max));
 	}
+}
+
+TEST_CASE("is equal operator") {
+	UnsignedBigInteger x{0};
+	UnsignedBigInteger y{1};
+	UnsignedBigInteger z{ std::numeric_limits<int>::max() };
+	REQUIRE(x==x);
+	REQUIRE_FALSE(x==y);
+	REQUIRE_FALSE(x==z);
+	REQUIRE(y==y);
+	REQUIRE_FALSE(y==x);
+	REQUIRE_FALSE(y==z);
+	REQUIRE(z==z);
+	REQUIRE_FALSE(z==x);
+	REQUIRE_FALSE(z==y);
+
 }
 
 int main(int argc, char* argv[]) {
